06e1472f9c1db527b94e6d01f34e27ebf6792a67
MUHAMMAD SAIDU ABDUL <muhammadsaidu92@gmail.com>
2024-05-29 11:51:27 -0700

    InitialCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { Home3Component } from './home3/home3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    Home3Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+       { path: 'login', component: LoginComponent },
+       { path: 'home3', component: Home3Component }]),
   ],
   providers: [
     provideClientHydration()
